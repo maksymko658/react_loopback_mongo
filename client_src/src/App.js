0c303c73a2ce9722044929064b53e0c0500058b2
maksymko658 <Jmaksy@ukr.net>
2017-10-25 17:02:31 +0300

    navbar

--- client_src/src/App.js
+++ client_src/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main';
+import Navbar from './components/navbar';
 
 const App = () => (
  <div>
+ <Navbar />
  <Main />
  </div>
   );
