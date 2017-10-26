19b3b803acacd28645572566d056a19aae37a7d4
maksymko658 <Jmaksy@ukr.net>
2017-10-26 11:40:42 +0300

    trying to put meetups in react

--- client_src/src/components/Meetups.js
+++ client_src/src/components/Meetups.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Meetups extends Component {
+	constructor(){
+		super();
+		this.state = {
+			meetups: []
+		}
+	}
+	componentWillMount(){
+		
+	}
+
+	getMeetups(){
+		axios.get('http://localhost:3000/api/meetups')
+		.then(response => {
+			console.log(response);
+		})
+	}
+
     render() {
         return (
             <div>
