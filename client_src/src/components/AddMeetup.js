d0c4467f26209f3dee7981634c67395665bc3a3f
maksymko658 <Jmaksy@ukr.net>
2017-11-02 18:49:24 +0200

    now all works

---
+++ client_src/src/components/AddMeetup.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+class AddMeetup extends Component {
+  AddMeetup(newMeetup){
+    // console.log(newMeetup);
+    axios.request({
+      method:'post',
+      url:'http://localhost:3000/api/meetups',
+      data: newMeetup
+    }).then(response => {
+      this.props.history.push('/');
+    }).catch(err => console.log(err));
+  }
+
+  onSubmit(e){
+    const newMeetup = {
+      name: this.refs.name.value,
+      city: this.refs.city.value,
+      adress: this.refs.adress.value,
+    }
+    this.AddMeetup(newMeetup)
+    e.preventDefault();
+  }
+
+    render() {
+        return (
+          <div>
+            <br />
+          <Link className="btn grey" to="/">Back</Link>
+            <h1>Add meetup</h1>
+               <form onSubmit={this.onSubmit.bind(this)}>
+              <div className="input-field">
+              <input type="text" name="name" ref="name" />
+              <label htmlFor="name">Name</label>
+              </div>
+
+                 <div className="input-field">
+              <input type="text" name="city" ref="city" />
+              <label htmlFor="city">City</label>
+              </div>
+
+                   <div className="input-field">
+              <input type="text" name="adress" ref="adress" />
+              <label htmlFor="adress">Address</label>
+              </div>
+                <input type="submit" value="Save" className="btn" />
+              </form>
+          </div>
+        );
+    }
+}
+
+export default AddMeetup;
---
+++ client_src/src/components/EditMeetup.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom';
+
+class EditMeetup extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      id:'',
+      name:'',
+      city:'',
+      adress:''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  componentWillMount(){
+    this.getMeetupDetails();
+  }
+
+  getMeetupDetails(){
+  let meetupId = this.props.match.params.id;
+      axios.get(`http://localhost:3000/api/meetups/${meetupId}`)
+    .then(response => {
+      this.setState({
+        id: response.data.id,
+        name: response.data.name,
+        city: response.data.city,
+        adress: response.data.adress
+      }, () => {
+        console.log(this.state);
+      });
+    })
+    .catch(err => console.log(err));
+}
+
+  editMeetup(newMeetup){
+        // console.log(newMeetup);
+    axios.request({
+      method:'put',
+      url:`http://localhost:3000/api/meetups/${this.state.id}`,
+      data: newMeetup
+    }).then(response => {
+      this.props.history.push('/');
+    }).catch(err => console.log(err));
+  }
+
+    onSubmit(e){
+    const newMeetup = {
+      name: this.refs.name.value,
+      city: this.refs.city.value,
+      adress: this.refs.adress.value,
+    }
+    this.editMeetup(newMeetup)
+    e.preventDefault();
+  }
+
+  handleInputChange(e){
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+    render() {
+        return (
+          <div>
+            <br />
+          <Link className="btn grey" to="/">Back</Link>
+            <h1>Edit</h1>
+               <form onSubmit={this.onSubmit.bind(this)}>
+              <div className="input-field">
+              <input type="text" name="name" ref="name" 
+              value={this.state.name} 
+              onChange={this.handleInputChange} />
+
+              <label htmlFor="name">Name</label>
+              </div>
+
+                 <div className="input-field">
+              <input type="text" name="city" ref="city" 
+              value={this.state.city} 
+              onChange={this.handleInputChange} />
+
+              <label htmlFor="city">City</label>
+              </div>
+
+                   <div className="input-field">
+              <input type="text" name="adress" ref="adress" 
+              value={this.state.adress}
+              onChange={this.handleInputChange} />
+
+              <label htmlFor="adress">Address</label>
+              </div>
+                <input type="submit" value="Save" className="btn" />
+              </form>
+          </div>
+        );
+    }
+}
+
+export default EditMeetup;
