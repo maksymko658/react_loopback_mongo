1f706d2e917a3e9808d6357010ca88801635e359
maksymko658 <Jmaksy@ukr.net>
2017-11-02 15:13:40 +0200

    add inputs

--- client_src/src/components/AddMeetup.js
+++ client_src/src/components/AddMeetup.js
@@ -4,10 +4,32 @@ import {Link} from 'react-router-dom';
 
 class AddMeetup extends Component {
 
+  onSubmit(){
+    return 1;
+  }
+
     render() {
         return (
           <div>
-          <h1>Add meetup</h1>
+            <br />
+          <Link className="btn grey" to="/">Back</Link>
+            <h1>Add meetup</h1>
+               <form onSubmit={this.onSubmit.bind(this)}>
+              <div className="input-field">
+              <input type="text" name="name" ref="name" />
+              <label htmlFor="name">Name</label>
+              </div>
+
+                 <div className="input-field">
+              <input type="text" name="city" ref="city" />
+              <label htmlFor="city">City</label>
+              </div>
+
+                   <div className="input-field">
+              <input type="text" name="adress" ref="adress" />
+              <label htmlFor="address">Address</label>
+              </div>
+              </form>
           </div>
         );
     }
